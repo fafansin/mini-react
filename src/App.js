4ded18403ba71e6de74f4e1b72d6578f1749909e
laviel <fafansin@gmail.com>
2024-08-07 12:29:37 +0300

    feat: created base files for login, home and signup pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
 import './App.css';
+
+import Login from './pages/login/Login'
 
 function App() {
   return (
     <div className="App">
-      
+      <Login/>
+      <Button variant="contained">Hello world</Button>
     </div>
   );
 }
---
+++ src/pages/login/Login.js
@@ -0,0 +1,13 @@
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+
+const Login = () => {
+  return (
+    <Paper>
+      <Typography variant="button">Login</Typography>
+    </Paper>
+    
+  )
+}
+
+export default Login
